211650bdcfbb1467ba18439d798e049f095e1a65
samad <samadarshad@hotmail.co.uk>
2021-03-18 19:09:29 +0000

    env var in docker

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log("Microservices rock!");
+	console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
